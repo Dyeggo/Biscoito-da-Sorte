d4c3022466e8edd49886c93e13df4fa9bb8bf842
Devdyeggo <dyeggo_drs@hotmail.com>
2022-01-25 23:31:34 -0300

    criando a estrutura do codigo e adicionando as imagem do projeto

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
-export default function App() {
+function App() {
+  const [img, setImg] = useState(require('./src/biscoito.png'));
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        source={img}
+        style={styles.img}
+      />
+
+      <Text style={styles.textofrase}>"esta é a primeira frase do biscoito!"</Text>
+
+      <TouchableOpacity style={styles.botao} onPress={() => alert('TESTE')}>
+        <View style={styles.btnArea}>
+          <Text style={styles.btnTexto}>Quebrar Biscoito</Text>
+        </View>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={[styles.botao, { marginTop: 15, borderColor: '#121212' }]} onPress={() => alert('TESTE')}>
+        <View style={styles.btnArea}>
+          <Text style={[styles.btnTexto, { color: '#121212' }]}>Reniciar Biscoito</Text>
+        </View>
+      </TouchableOpacity>
+
     </View>
   );
 }
@@ -13,8 +34,38 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center'
   },
-});
+  img: {
+    width: 230,
+    height: 230,
+  },
+  textofrase: {
+    fontSize: 20,
+    color: '#dd7b22',
+    margin: 30,
+    fontStyle: 'italic',
+    textAlign: 'center'
+  },
+  botao: {
+    width: 200,
+    height: 50,
+    borderColor: '#dd7b22',
+    borderWidth: 2,
+    borderRadius: 25
+  },
+  btnArea: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  btnTexto: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#dd7b22'
+  },
+
+})
+
+export default App;
