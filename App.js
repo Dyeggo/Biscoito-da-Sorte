0e9df8c4ca262803b14cd8d706384ba5beca37cd
Devdyeggo <dyeggo_drs@hotmail.com>
2022-01-26 22:26:10 -0300

    implementação das frases e a troca de foto do biscoito

--- App.js
+++ App.js
@@ -3,6 +3,31 @@ import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
 function App() {
   const [img, setImg] = useState(require('./src/biscoito.png'));
+  const [textoFrase, setTextoFrase] = useState('');
+
+  let frases = [
+    'A vida trará coisas boas se tiver paciência.',
+    'Demonstre amor e alegria em todas as oportunidades e verá que a paz nasce dentro de si.',
+    'Não compense na ira o que lhe falta na razão.',
+    ' Defeitos e virtudes são apenas dois lados da mesma moeda.',
+    ' A maior de todas as torres começa no solo.',
+    ' Não há que ser forte. Há que ser flexível.',
+    ' Todos os dias organiza os seus cabelos, por que não faz o mesmo com o coração?'
+
+  ];
+
+  function quebraBiscoito() {
+    let numeroAleatorio = Math.floor(Math.random() * frases.length)
+
+    setTextoFrase('" ' + frases[numeroAleatorio] + ' "');
+    setImg(require('./src/biscoitoAberto.png'));
+
+  }
+
+  function reniciarBiscoito() {
+    setTextoFrase('');
+    setImg(require('./src/biscoito.png'));
+  }
 
 
 
@@ -13,15 +38,15 @@ function App() {
         style={styles.img}
       />
 
-      <Text style={styles.textofrase}>"esta é a primeira frase do biscoito!"</Text>
+      <Text style={styles.textofrase}>{textoFrase}</Text>
 
-      <TouchableOpacity style={styles.botao} onPress={() => alert('TESTE')}>
+      <TouchableOpacity style={styles.botao} onPress={quebraBiscoito}>
         <View style={styles.btnArea}>
           <Text style={styles.btnTexto}>Quebrar Biscoito</Text>
         </View>
       </TouchableOpacity>
 
-      <TouchableOpacity style={[styles.botao, { marginTop: 15, borderColor: '#121212' }]} onPress={() => alert('TESTE')}>
+      <TouchableOpacity style={[styles.botao, { marginTop: 15, borderColor: '#121212' }]} onPress={reniciarBiscoito}>
         <View style={styles.btnArea}>
           <Text style={[styles.btnTexto, { color: '#121212' }]}>Reniciar Biscoito</Text>
         </View>
